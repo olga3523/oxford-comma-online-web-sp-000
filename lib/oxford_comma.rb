be07a63ecbd0319fc93d95cbd0e897f44e875932
olga gomelskiy <olichka23.85@gmail.com>
2021-04-13 03:49:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 def oxford_comma(array)
   
-  case array.length 
+  case array.length
 when 1
   "#{array[0]}"
 when 2
   array[0..1].join(" and ")
-else 
+else
   array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
-end 
+end
+end
 
 end
