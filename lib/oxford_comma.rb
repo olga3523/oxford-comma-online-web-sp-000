7ce8061b67f017de2a6d6b6a0ecc91325b257bdc
olga gomelskiy <olichka23.85@gmail.com>
2021-04-13 03:52:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,14 @@
 def oxford_comma(array)
-  
-  case array.length
-when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end
-end
+    if array.length == 1
+      return "#{array[0]}"
+    elsif array.length == 2
+      return array.join(" and ")
+    elsif array.length >= 3
+      new_last_array_item = "and #{array[-1]}"
+      array.pop
+      array.push(new_last_array_item)
+      return array.join(", ")
+    end
+  end
 
 end
